9c4884f0efa5887c1aa0e5b26c30e90b282c39f4
Tim Wagner <t.wagner@techdivision.com>
2019-11-14 22:42:44 +0100

    Refactoring cache integration

--- src/Utils/ParamNames.php
+++ src/Utils/ParamNames.php
@@ -23,19 +23,13 @@ namespace TechDivision\Import\Customer\Utils;
 /**
  * Utility class containing parameter names for prepared statements.
  *
- * @author    Tim Wagner <t.wagner@techdivision.com>
- * @copyright 2018 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link      https://github.com/techdivision/import-customer
- * @link      http://www.techdivision.com
+ * @author     Tim Wagner <t.wagner@techdivision.com>
+ * @copyright  2018 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/techdivision/import-customer
+ * @link       http://www.techdivision.com
+ * @deprecated Since 13.0.0 use MemberNames instead
  */
 class ParamNames extends \TechDivision\Import\Customer\Utils\MemberNames
 {
-
-    /**
-     * Name for the parameter name 'pk'.
-     *
-     * @var string
-     */
-    const PK = 'pk';
 }
